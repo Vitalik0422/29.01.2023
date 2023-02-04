91826315c3c6a74e2d1f8d7261dfd59209c6f761
Vitalik0422 <vitaliy19980422@gmail.com>
2023-02-04 19:49:43 +0200

    branch

--- js/main.js
+++ js/main.js
@@ -1,38 +1,84 @@
 const gender = document.querySelector('.gender');
 const massege = document.querySelector('.message');
 const date = document.querySelector('.date');
+const dateTitle = document.querySelector('.dateTitle')
+const name = document.querySelector('.name');
+const inpuToNamePeople = document.querySelector('.inpuToNamePeople')
+
 const dateNow = new Date();
-let age = Number();
+const age = Number();
+let genderSelect = true;
+let firstName;
+let surename;
+
 
 
 ///////model
 
 /////view
-
-
-////////controler
-
-gender.addEventListener('change', (ev) => {
-    let status = ev.target.value;
-    console.log(status);
-    if(ev.target.value == 1){
-        massege.innerHTML = ' ';
-        date.classList.add('on');
-    }
-    else if(ev.target.value == 2){
-        massege.innerHTML = 'Дівчат вік не запитують'
-        date.classList.remove('on')
-    }
-    else  {
-        date.classList.remove('on')
-        massege.innerHTML = 'Виберіть будь ласка стать!';
-    }
-    return
-});
-
 date.addEventListener('change', (ev) => {
     age = Number(ev.target.value.substr(0, 4));
     console.log( dateNow );
 
     massege.innerHTML = dateNow.getFullYear() - age + ' Рочки';
+
 });
+
+const girlAlert = () => {
+    massege.innerHTML = `${firstName}, у дівчат вік не питають)`
+}
+
+const girlAlertoff = () => {
+    massege.innerHTML = " "
+}
+
+////////controler
+
+gender.addEventListener('change', (ev) => {
+    console.log ('Gender', ev.target.value )
+    return ev.target.value == 1 ? genderSelect = true : genderSelect = false, yearsOldOff();
+    
+})
+
+
+inpuToNamePeople[0].addEventListener('change', (ev) =>{
+    firstName = ev.target.value.charAt(0).toUpperCase() + ev.target.value.slice(1).toLowerCase();
+    console.log(firstName.length)
+    check()
+    return firstName;
+})
+inpuToNamePeople[1].addEventListener('change', (ev) =>{
+    
+    surename = ev.target.value.charAt(0).toUpperCase() + ev.target.value.slice(1).toLowerCase();
+    console.log(surename.length)
+    check()
+    return surename;
+})
+
+const check = () => {
+    console.log(firstName)
+    console.log(genderSelect)
+    if (firstName.length == 0){
+        return check;
+    }
+     else if (surename.length == 0){
+        return check;
+    } else {
+        dateInput();
+    }
+}
+
+const dateInput = () => {
+    return (genderSelect === true) ? yearsOld() : girlAlert();
+}
+
+const yearsOld = () => {
+    date.classList.add('on')
+    dateTitle.classList.add('onn')
+}
+
+const yearsOldOff = () => {
+    date.classList.remove('on')
+    dateTitle.classList.remove('onn')
+}
+
